2b26fc3668ef1282938c943da59f5d09770d4ea7
Grant Seward <sewardgw@gmail.com>
2021-01-27 00:00:46 -0500

    updated pr comment

--- scripts/deploy_2.sh
+++ scripts/deploy_2.sh
@@ -3,18 +3,18 @@
 # Script parameters
 STACK_NAME=tree-schema
 ENV_STAGE_NAME=prod
-VPC_ID='vpc-08f519d04ca17fd96'
-APP_SECURITY_GROUPS='sg-0696c0e5240ce9252'
-LOAD_BALANCER_SECURITY_GROUPS='sg-0a1d73dbb9e153d05'
-APP_SUBNETS='subnet-0a36c9c4b8f0752f3,subnet-0c51af3048d90ba16'
-LOAD_BALANCER_SUBNETS='subnet-05286657db5291d4c,subnet-050d577e527203da4'
-LOAD_BALANCER_CERT_ARN='arn:aws:acm:us-east-1:656100572794:certificate/c5303b3a-d3c8-44e3-a204-497811be6073'
-SECRETS_ARN='arn:aws:secretsmanager:us-east-1:656100572794:secret:tree-schema-secret-poI9VH'
-FROM_EMAIL_ADDRESS='asher@treeschema.com'
-KMS_KEY_ARN='arn:aws:kms:us-east-1:656100572794:key/7e1d9f18-bae3-4068-ac51-96b4e4cdd6bf'
-TREE_SCHEMA_IMAGE='215001001805.dkr.ecr.us-east-1.amazonaws.com/tree-schema-data-catalog-app:0.0.1'
-TREE_SCHEMA_NGINX_IMAGE='215001001805.dkr.ecr.us-east-1.amazonaws.com/tree-schema-data-catalog-nginx:0.0.1'
-TREE_SCHEMA_API_IMAGE='215001001805.dkr.ecr.us-east-1.amazonaws.com/tree-schema-data-catalog-api:0.0.1'
+VPC_ID=''
+APP_SECURITY_GROUPS=''
+LOAD_BALANCER_SECURITY_GROUPS=''
+APP_SUBNETS=''
+LOAD_BALANCER_SUBNETS=''
+LOAD_BALANCER_CERT_ARN=''
+SECRETS_ARN=''
+FROM_EMAIL_ADDRESS=''
+KMS_KEY_ARN=''
+TREE_SCHEMA_IMAGE='656100572794.dkr.ecr.us-east-1.amazonaws.com/tree-schema-data-catalog-app:0.0.1'
+TREE_SCHEMA_NGINX_IMAGE='656100572794.dkr.ecr.us-east-1.amazonaws.com/tree-schema-data-catalog-nginx:0.0.1'
+TREE_SCHEMA_API_IMAGE='656100572794.dkr.ecr.us-east-1.amazonaws.com/tree-schema-data-catalog-api:0.0.1'
 
 # Allow overriding profile
 if [ "$1" ]; then
